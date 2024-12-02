bbb2a81e94eb24a206f6a699ab3e065f793a2997
frozzun <eonjun0783@naver.com>
2024-12-02 15:22:58 +0900

    fix gitignore

--- init.sql
+++
@@ -1,7 +0,0 @@
--- 데이터베이스 생성
-CREATE DATABASE IF NOT EXISTS lawmon;
-
--- 사용자 생성 및 권한 부여
-CREATE USER IF NOT EXISTS 'qnlove'@'%' IDENTIFIED BY 'chanyoup1@';
-GRANT ALL PRIVILEGES ON lawmon.* TO 'qnlove'@'%';
-FLUSH PRIVILEGES;
