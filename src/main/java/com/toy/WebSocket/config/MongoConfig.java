f52ab26b5fb8c11d8e67c15ef3afd3d303c0d535
frozzun <eonjun0783@naver.com>
2024-12-30 16:44:24 +0900

    build.gradle update

---
+++ src/main/java/com/toy/WebSocket/config/MongoConfig.java
@@ -0,0 +1,31 @@
+package com.toy.WebSocket.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.convert.DbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@Configuration
+@EnableMongoRepositories
+public class MongoConfig {
+  @Autowired
+  private MongoMappingContext mongoMappingContext;
+
+  @Bean
+  public MappingMongoConverter mappingMongoConverter(
+    MongoDatabaseFactory mongoDatabaseFactory,
+    MongoMappingContext mongoMappingContext
+  ) {
+    DbRefResolver dbRefResolver = new DefaultDbRefResolver(mongoDatabaseFactory);
+    MappingMongoConverter mappingConverter = new MappingMongoConverter(dbRefResolver, mongoMappingContext);
+    mappingConverter.setTypeMapper(new DefaultMongoTypeMapper(null));
+    return mappingConverter;
+  }
+}
+
