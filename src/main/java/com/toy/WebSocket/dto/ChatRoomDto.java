883a764952a61eae81bde74f33c27b69e792be54
frozzun <eonjun0783@naver.com>
2024-12-30 20:00:58 +0900

    여러대의 채팅서버간에 메시지 공유하기 by Redis pub/sub

--- src/main/java/com/toy/WebSocket/dto/ChatRoomDto.java
+++ src/main/java/com/toy/WebSocket/dto/ChatRoomDto.java
@@ -3,11 +3,17 @@ package com.toy.WebSocket.dto;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.UUID;
 
 @Getter
 @Builder
-public class ChatRoomDto {
+public class ChatRoomDto implements Serializable {
+
+  @Serial
+  private static final long serialVersionUID = 6494678977089006639L;
+
   private String roomId;
   private String name;
 
