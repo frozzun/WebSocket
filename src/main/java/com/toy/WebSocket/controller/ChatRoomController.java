28f9d2d386f60f125d6a83d2f4550fa9cd0f4a07
frozzun <eonjun0783@naver.com>
2024-12-30 19:26:29 +0900

    Stomp로 채팅서버 고도화 하기

---
+++ src/main/java/com/toy/WebSocket/controller/ChatRoomController.java
@@ -0,0 +1,64 @@
+package com.toy.WebSocket.controller;
+
+import com.toy.WebSocket.dto.ChatRoomDto;
+import com.toy.WebSocket.entity.ChatRoom;
+import com.toy.WebSocket.repository.ChatRoomRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/chat")
+public class ChatRoomController {
+
+  private final ChatRoomRepository chatRoomRepository;
+
+  // 채팅 리스트 화면
+  @GetMapping("/room")
+  public String rooms(Model model) {
+    return "/chat/room";
+  }
+  // 모든 채팅방 목록 반환
+  @GetMapping("/rooms")
+  @ResponseBody
+  public List<ChatRoomDto> room() {
+    List<ChatRoom> chatRooms =  chatRoomRepository.findAllRoom();
+    return chatRooms.stream()
+      .map(chatRoom -> ChatRoomDto.builder()
+        .name(chatRoom.getName())
+        .roomId(chatRoom.getRoomId())
+        .build())  // 필요한 필드들로 변환
+      .collect(Collectors.toList());
+  }
+  // 채팅방 생성
+  @PostMapping("/room")
+  @ResponseBody
+  public ChatRoomDto createRoom(@RequestParam String name) {
+    ChatRoom chatRoom =  chatRoomRepository.createChatRoom(name);
+    return ChatRoomDto.builder()
+      .name(chatRoom.getName())
+      .roomId(chatRoom.getRoomId())
+      .build();
+  }
+  // 채팅방 입장 화면
+  @GetMapping("/room/enter/{roomId}")
+  public String roomDetail(Model model, @PathVariable String roomId) {
+    model.addAttribute("roomId", roomId);
+    return "/chat/roomdetail";
+  }
+  // 특정 채팅방 조회
+  @GetMapping("/room/{roomId}")
+  @ResponseBody
+  public ChatRoomDto roomInfo(@PathVariable String roomId) {
+    ChatRoom chatRoom =  chatRoomRepository.findRoomById(roomId);
+    return ChatRoomDto.builder()
+      .name(chatRoom.getName())
+      .roomId(chatRoom.getRoomId())
+      .build();
+  }
+}
--- src/main/java/com/toy/WebSocket/dto/ChatRoomDto.java
+++ src/main/java/com/toy/WebSocket/dto/ChatRoomDto.java
@@ -1,34 +1,20 @@
 package com.toy.WebSocket.dto;
 
-import com.toy.WebSocket.service.ChatService;
 import lombok.Builder;
 import lombok.Getter;
-import org.springframework.web.socket.WebSocketSession;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.UUID;
 
 @Getter
+@Builder
 public class ChatRoomDto {
   private String roomId;
   private String name;
-  private Set<WebSocketSession> sessions = new HashSet<>();
 
-  @Builder
-  public ChatRoomDto(String roomId, String name) {
-    this.roomId = roomId;
-    this.name = name;
-  }
-
-  public void handleActions(WebSocketSession session, ChatMessageDto chatMessageDto, ChatService chatService) {
-    if (chatMessageDto.getType().equals(ChatMessageDto.MessageType.ENTER)) {
-      sessions.add(session);
-      chatMessageDto.setMessage(chatMessageDto.getSender() + "님이 입장했습니다.");
-    }
-    sendMessage(chatMessageDto, chatService);
-  }
-
-  public <T> void sendMessage(T message, ChatService chatService) {
-    sessions.parallelStream().forEach(session -> chatService.sendMessage(session, message));
+  public static ChatRoomDto create(String name) {
+    return ChatRoomDto.builder()
+      .roomId(UUID.randomUUID().toString())
+      .name(name)
+      .build();
   }
 }
