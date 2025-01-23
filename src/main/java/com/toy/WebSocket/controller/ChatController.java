b1ba0b7dc7530189a72660e42a71c7a535f7f5c0
frozzun <eonjun0783@naver.com>
2025-01-23 20:26:33 +0900

    채팅 메세지 기능 재 구현

--- src/main/java/com/toy/WebSocket/controller/ChatController.java
+++ src/main/java/com/toy/WebSocket/controller/ChatController.java
@@ -1,24 +1,27 @@
 package com.toy.WebSocket.controller;
 
 import com.toy.WebSocket.dto.ChatMessageDto;
+import com.toy.WebSocket.service.ChatMessageService;
 import com.toy.WebSocket.service.ChatRoomService;
 import com.toy.WebSocket.service.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 
+@Slf4j
 @RequiredArgsConstructor
 @Controller
 public class ChatController {
 
   private final ChatRoomService chatRoomService;
+  private final ChatMessageService chatMessageService;
   private final JwtTokenProvider jwtTokenProvider;
-  private final RedisTemplate redisTemplate;
+  private final RedisTemplate<String, Object> redisTemplate;
   private final ChannelTopic channelTopic;
-//  private final ChatRoomRedisRepo chatRoomRepository;
 
   /**
    * websocket "/pub/chat/message"로 들어오는 메시징을 처리한다.
@@ -32,7 +35,10 @@ public class ChatController {
       messageDto.setSender("[알림]");
       messageDto.setMessage(nickname + "님이 입장하셨습니다.");
     }
-    // Websocket에 발행된 메시지를 redis로 발행한다(publish)
-    redisTemplate.convertAndSend(channelTopic.getTopic(), messageDto);
+    chatMessageService.publishChatMessage(messageDto);
+    log.info("published Chat Message: {}", messageDto);
+
+//    // Websocket에 발행된 메시지를 redis로 발행한다(publish)
+//    redisTemplate.convertAndSend(channelTopic.getTopic(), messageDto);
   }
 }
